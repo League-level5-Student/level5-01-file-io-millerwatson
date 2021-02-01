d3e928a004faace86d8c1cf9f1f6369689ab334c
millerwatson <millerwatson@users.noreply.github.com>
2021-02-01 13:01:53 -0800

    Finished DictionaryIterator

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -1,6 +1,10 @@
 package _04_Directory_Iteration;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 
 import javax.swing.JFileChooser;
 
@@ -10,18 +14,19 @@ public class DirectoryIterator {
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
-		JFileChooser jfc = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		int returnVal = jfc.showOpenDialog(null);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			File directory = jfc.getSelectedFile();
-			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
-				}
+//		JFileChooser jfc = new JFileChooser();
+//		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+//		int returnVal = jfc.showOpenDialog(null);
+//		if (returnVal == JFileChooser.APPROVE_OPTION) {
+		DirectoryIterator di = new DirectoryIterator();
+		File directory = new File("src");
+		File[] files = directory.listFiles();
+		if(files != null) {
+			for(File f : files) {
+				di.copyrightFile(f);
 			}
 		}
+		
 		
 		/*
 		 * Your task is to write a program that iterates through the src folder of this current Java Project. 
@@ -30,4 +35,28 @@ public class DirectoryIterator {
 		 * (e.g //Copyright © 2019 FirstName LastName)
 		 */
 	}
+	
+	void copyrightFile(File f) {
+		String filePath = f.getPath();
+		try {
+			if (filePath.substring(filePath.indexOf('.'), filePath.length()).equals(".java")) {
+				try {
+					FileWriter fw = new FileWriter(filePath, true);
+					fw.append("//This is code from Miller!");
+					fw.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (StringIndexOutOfBoundsException e) {
+			File[] newFiles = f.listFiles();
+			for (int i = 0; i < newFiles.length; i++) {
+				copyrightFile(newFiles[i]);
+			}
+		}
+		
+	}
 }
+//This is code from Miller!//This is code from Miller!
